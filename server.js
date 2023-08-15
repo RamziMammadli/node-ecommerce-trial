538fb765fb2bcf54d73ca80c521fbf6b1cedc554
RamziMammadli <remzimemmedli95@gmail.com>
2023-08-15 09:51:39 +0400

    json done

--- server.js
+++ server.js
@@ -7,7 +7,6 @@ const app = express();
 
 app.use(bodyParser.json());
 
-// JSON Server'ı başlatma
 const jsonServerRouter = jsonServer.router('db.json');
 const middlewares = jsonServer.defaults();
 app.use(middlewares);
@@ -16,52 +15,42 @@ app.use('/images', express.static('public/images'));
 
 
 
-// Ürünleri almak
+// mehsullar
 app.get('/api/products', (req, res) => {
-  // Burada ürünleri gerçek bir veritabanından alabilirsiniz
-  const products = []; // Gerçek ürün verilerini burada doldurun
+  const products = []; 
   res.json(products);
 });
 
-// Sepeti almak
+// Sebet
 app.get('/api/basket', (req, res) => {
-  // Burada sepeti gerçek bir veritabanından alabilirsiniz
-  const basket = []; // Gerçek sepet verilerini burada doldurun
+  const basket = []; 
   res.json(basket);
 });
 
-// Favorileri almak
+// wishlist
 app.get('/api/favourites', (req, res) => {
-  // Burada favorileri gerçek bir veritabanından alabilirsiniz
-  const favourites = []; // Gerçek favori verilerini burada doldurun
+  const favourites = []; 
   res.json(favourites);
 });
 
-// Ürün ekleme
+// mehsul elave et
 app.post('/api/products', (req, res) => {
-  // Ürünü req.body'den alın ve veritabanına ekleyin
   const newProduct = req.body;
-  // Gerçek bir veritabanına eklemek için gerekli kodu burada kullanın
   res.json(newProduct);
 });
 
-// Sepete ekleme
+// sebete elave et
 app.post('/api/basket', (req, res) => {
-  // Ürünü req.body'den alın ve sepete ekleyin
   const newBasketItem = req.body;
-  // Gerçek bir veritabanına eklemek için gerekli kodu burada kullanın
   res.json(newBasketItem);
 });
 
-// Favorilere ekleme
+// add wishlist
 app.post('/api/favourites', (req, res) => {
-  // Ürünü req.body'den alın ve favorilere ekleyin
   const newFavourite = req.body;
-  // Gerçek bir veritabanına eklemek için gerekli kodu burada kullanın
   res.json(newFavourite);
 });
 
-// Express sunucusunu başlatma
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
